f7113b3bfa94a5d436d7208e6a68d6096cb99832
FShaaM <zinovevas135@gmail.com>
2025-10-20 12:40:41 +0300

    что-то 2

--- master.cpp
+++ master.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 
+int** creat (size_t row, size_t col);
+
+void remove (int** m, size_t row, size_t col);
+
 int main {
 
 size_t row = 0, row = 0;
@@ -8,6 +12,8 @@ std::cin >> row >> col;
 
 if (!std::cin)
 	return 1;
+
+
 
 std::cout << row << " " << col; << '\n';
 
