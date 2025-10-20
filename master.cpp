9a8f33157593466bdd1fa0f05aee4067867af58b
FShaaM <zinovevas135@gmail.com>
2025-10-20 18:20:19 +0300

    с домашним заданием

--- master.cpp
+++ master.cpp
@@ -8,6 +8,8 @@ void input(int** m, size_t row, size_t col);
 
 void output(const int* const* m, size_t row, size_t col);
 
+int** convert (const int* t, size_t n, const size_t* lns, size_t rows);
+
 int main () {
 
 	size_t row = 0, col = 0;
@@ -16,16 +18,25 @@ int main () {
 	
 	if (!std::cin)
 		return 1;
-	
+
+	try {
+
 	int** m = create(row, col);
-		
-		input(m, row, col);
+
+	 }
+        catch (...) {
+                return 2;
+        }
+
+	input(m, row, col);
+
 	if (!std::cin) {
-			remove(m, row);
-        return 1;
+		remove(m, row);
+		return 1;
 	}
-	
+
 	output(m, row, col);
+
 	remove(m, row);
 	
 	std::cout << '\n' << row << " " << col << '\n';
@@ -38,6 +49,7 @@ void input(int** m, size_t row, size_t col) {
     for (size_t i = 0; i < row && (std::cin); ++i)
         for (size_t j = 0; j < col && (std::cin); ++j)
             std::cin >> m[i][j];
+     std::cout << '\n';
 }
 
 void output(const int* const* m, size_t row, size_t col) {
@@ -67,7 +79,8 @@ int** create(size_t row, size_t col) {
     catch (...) {
 
             remove(result, i);
-            throw;
+
+	    throw;
     }
 
         return result;
@@ -83,3 +96,27 @@ void remove(int** m, size_t row) {
     delete[] m;
 
 }
+
+
+int** convert (const int* t, size_t n, const size_t* lns, size_t rows) {
+
+	int** arr = new int* [rows];
+
+	try{
+	
+	for (size_t k = 0; k < rows; ++k)
+	      arr[k] = new int [lns[k]];
+	}
+        catch (...) {
+                remove (arr, rows);
+		throw;
+	}
+
+        for (size_t k = 0, i = 0; k < rows; ++k)
+		for (size_t j = 0; j < lns[k] && i < n; ++j, ++i)
+			arr[k][j] = t[i];
+
+	return arr;
+
+}
+
